dcd25beb8d451ace165d4ccfcdd24e2dabed9edc
Noe <noe.duruz@gmail.com>
2024-12-20 12:02:29 +0100

    day16-partial

--- 2024/day16/src/main.rs
+++ 2024/day16/src/main.rs
@@ -137,7 +137,88 @@ fn part1(input: &str) -> usize {
 }
 
 fn part2(input: &str) -> usize {
-    0
+    let (s, e, mut map) = parse_input(input);
+
+    let mut queue = vec![(s, 0, Direction::Right)];
+    let mut min_cost = usize::MAX;
+
+    while let Some((pos, cost, prev_dir)) = queue.pop() {
+        if pos == e {
+            min_cost = min_cost.min(cost);
+            continue;
+        }
+
+        for dir in [
+            Direction::Up,
+            Direction::Down,
+            Direction::Left,
+            Direction::Right,
+        ] {
+            let mut new_pos = pos.clone();
+            new_pos.move_dir(&dir);
+
+            let new_cost = {
+                if prev_dir == dir {
+                    cost + 1
+                } else {
+                    cost + 1001
+                }
+            };
+
+            if let Tile::Empty {
+                up,
+                down,
+                left,
+                right,
+            } = &mut map[new_pos.y][new_pos.x]
+            {
+                if dir == Direction::Up && *up > new_cost {
+                    *up = new_cost;
+                    queue.push((new_pos, new_cost, dir));
+                } else if dir == Direction::Down && *down > new_cost {
+                    *down = new_cost;
+                    queue.push((new_pos, new_cost, dir));
+                } else if dir == Direction::Left && *left > new_cost {
+                    *left = new_cost;
+                    queue.push((new_pos, new_cost, dir));
+                } else if dir == Direction::Right && *right > new_cost {
+                    *right = new_cost;
+                    queue.push((new_pos, new_cost, dir));
+                }
+            }
+        }
+    }
+
+    let mut total = 1;
+    let mut queue = vec![e];
+
+    while let Some(pos) = queue.pop() {
+        if let Tile::Empty {
+            up,
+            down,
+            left,
+            right,
+        } = map[pos.y][pos.x]
+        {
+            let min_dir = [(
+                (Direction::Up, up),
+                (Direction::Down, down),
+                (Direction::Left, left),
+                (Direction::Right, right),
+            )]
+            .iter()
+            .fold((Vec::new(), 0), |(mut dirs, mut min_cost), (dir, c)| {
+                if dirs.is_empty() || c < min_cost {
+                    dirs.clear();
+                    dirs.push(dir);
+                    min_cost = c;
+                } else if min_cost == c {
+                    dirs.push(dir);
+                }
+            });
+        }
+    }
+    min_cost
 }
 
 fn main() {
@@ -197,5 +278,26 @@ fn test_small() {
 
     assert_eq!(part1(&input), 11048);
 
-    // assert_eq!(part2(&input, 11, 7), 0);
+    assert_eq!(part2(&input), 45);
+
+    let input = "\
+#################
+#...#...#...#..E#
+#.#.#.#.#.#.#.#.#
+#.#.#.#...#...#.#
+#.#.#.#.###.#.#.#
+#...#.#.#.....#.#
+#.#.#.#.#.#####.#
+#.#...#.#.#.....#
+#.#.#####.#.###.#
+#.#.#.......#...#
+#.#.###.#####.###
+#.#.#...#.....#.#
+#.#.#.#####.###.#
+#.#.#.........#.#
+#.#.#.#########.#
+#S#.............#
+#################";
+
+    assert_eq!(part2(&input), 64);
 }
