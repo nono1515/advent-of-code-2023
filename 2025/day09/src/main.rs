b5a4453460602b43d7b1fefb613e0b9e0735cdef
Noe <noe.duruz@gmail.com>
2025-12-23 14:19:06 +0100

    day09

--- 2025/day09/src/main.rs
+++ 2025/day09/src/main.rs
@@ -1,11 +1,15 @@
+use std::collections::HashMap;
+
+type Tile = [u64; 2];
+
 fn part1(input: &str) -> u64 {
     let corners = parse_input(&input);
     (0..corners.len())
         .flat_map(|i| {
-            let (x1, y1) = corners[i];
+            let [x1, y1] = corners[i];
             ((i + 1)..corners.len())
                 .map(|j| {
-                    let (x2, y2) = corners[j];
+                    let [x2, y2] = corners[j];
                     (x1.max(x2) - x1.min(x2) + 1) * (y1.max(y2) - y1.min(y2) + 1)
                 })
                 .max()
@@ -40,18 +44,24 @@ enum Wall {
 
 fn part2(input: &str) -> u64 {
     let corners = parse_input(input);
-    let xsize = corners.iter().map(|(x, _)| *x).max().unwrap() as usize;
-    let ysize = corners.iter().map(|(_, y)| *y).max().unwrap() as usize;
-
-    let mut borders = vec![vec![Wall::Empty; xsize + 1]; ysize + 1];
-    for ((c1, c2), c3) in corners
+    let x_mapping = shrink(&corners, 0);
+    let y_mapping = shrink(&corners, 1);
+    let corners_mapped = corners
         .iter()
-        .zip(corners.iter().cycle().skip(1))
-        .zip(corners.iter().cycle().skip(2))
+        .map(|c| [x_mapping[&c[0]], y_mapping[&c[1]]])
+        .collect::<Vec<_>>();
+
+    let xsize = corners_mapped.iter().map(|c| c[0]).max().unwrap() as usize;
+    let ysize = corners_mapped.iter().map(|c| c[1]).max().unwrap() as usize;
+    let mut borders = vec![vec![Wall::Empty; xsize + 1]; ysize + 1];
+    for ((c1, c2), c3) in corners_mapped
+        .iter()
+        .zip(corners_mapped.iter().cycle().skip(1))
+        .zip(corners_mapped.iter().cycle().skip(2))
     {
-        let (x1, y1) = (c1.0 as i32, c1.1 as i32);
-        let (x2, y2) = (c2.0 as i32, c2.1 as i32);
-        let (x3, y3) = (c3.0 as i32, c3.1 as i32);
+        let (x1, y1) = (c1[0] as i32, c1[1] as i32);
+        let (x2, y2) = (c2[0] as i32, c2[1] as i32);
+        let (x3, y3) = (c3[0] as i32, c3[1] as i32);
         let from_prev = (y2 - y1, x2 - x1);
         let to_next = (y3 - y2, x3 - x2);
         let corner_type = match (from_prev, to_next) {
@@ -69,7 +79,7 @@ fn part2(input: &str) -> u64 {
             },
             _ => unreachable!(),
         };
-        borders[c2.1 as usize][c2.0 as usize] = corner_type;
+        borders[c2[1] as usize][c2[0] as usize] = corner_type;
         match to_next {
             (0, _) => {
                 for x in x2.min(x3)..=x2.max(x3) {
@@ -85,22 +95,22 @@ fn part2(input: &str) -> u64 {
         };
     }
 
-    // for l in &borders {
-    //     for e in l {
-    //         let tile = match e {
-    //             Wall::UpperLeft => '┌',
-    //             Wall::UpperRight => '┐',
-    //             Wall::LowerLeft => '└',
-    //             Wall::LowerRight => '┘',
-    //             Wall::Vertical => '|',
-    //             Wall::Horizontal => '-',
-    //             Wall::Empty => '.',
-    //         };
-    //         print!("{tile}");
-    //     }
-    //     println!();
-    // }
-    // println!();
+    for l in &borders {
+        for e in l {
+            let tile = match e {
+                Wall::UpperLeft => '┌',
+                Wall::UpperRight => '┐',
+                Wall::LowerLeft => '└',
+                Wall::LowerRight => '┘',
+                Wall::Vertical => '|',
+                Wall::Horizontal => '-',
+                Wall::Empty => '.',
+            };
+            print!("{tile}");
+        }
+        println!();
+    }
+    println!();
 
     let matrix = (0..=ysize)
         .map(|y| {
@@ -136,21 +146,20 @@ fn part2(input: &str) -> u64 {
         })
         .collect::<Vec<_>>();
 
-    println!("Filling done");
-    // for l in &matrix {
-    //     for e in l {
-    //         if *e {
-    //             print!("#");
-    //         } else {
-    //             print! {"."};
-    //         }
-    //     }
-    //     println!();
-    // }
+    for l in &matrix {
+        for e in l {
+            if *e {
+                print!("#");
+            } else {
+                print! {"."};
+            }
+        }
+        println!();
+    }
 
-    (0..corners.len())
+    (0..corners_mapped.len())
         .flat_map(|i| {
-            let (x1, y1) = corners[i];
+            let [x1, y1] = corners[i];
             ((i + 1)..corners.len())
                 .filter_map(|j| {
                     let (x2, y2) = corners[j];
@@ -171,69 +180,24 @@ fn part2(input: &str) -> u64 {
         .unwrap()
 }
 
-fn update_state(
-    is_inside: &mut bool,
-    wall_dir: &mut Option<WallDirection>,
-    x: usize,
-    y: usize,
-    corners: &Vec<(u64, u64)>,
-) {
-    let (x, y) = (x as u64, y as u64);
-    for (c1, c2) in corners.iter().zip(corners.iter().cycle().skip(1)) {
-        if let Some(dir) = wall_dir {
-            if *c1 == (x, y) && c2.1 != y {
-                if (*dir == WallDirection::Up && c2.1 > y)  // the previous wall is upward
-                    || (*dir == WallDirection::Down && c2.1 < y)
-                {
-                    *is_inside = !*is_inside;
-                }
-                *wall_dir = None;
-                break;
-            }
-            if *c2 == (x, y) && c1.1 != y {
-                if (*dir == WallDirection::Up && c1.1 > y)  // the previous wall is upward
-                    || (*dir == WallDirection::Down && c1.1 < y)
-                {
-                    *is_inside = !*is_inside;
-                }
-                *wall_dir = None;
-                break;
-            }
-        } else {
-            if *c1 == (x, y) {
-                if c2.1 < y {
-                    *wall_dir = Some(WallDirection::Up);
-                } else if c2.1 > y {
-                    *wall_dir = Some(WallDirection::Down);
-                } else {
-                    continue;
-                }
-                break;
-            } else if *c2 == (x, y) {
-                if c1.1 < y {
-                    *wall_dir = Some(WallDirection::Up);
-                } else if c1.1 > y {
-                    *wall_dir = Some(WallDirection::Down);
-                } else {
-                    continue;
-                }
-                break;
-            } else if (c1.0.min(c2.0)..=c1.0.max(c2.0)).contains(&x)
-                && (c1.1.min(c2.1)..=c1.1.max(c2.1)).contains(&y)
-            {
-                *is_inside = !*is_inside;
-                break;
-            }
-        }
-    }
+fn shrink(corners: &[Tile], index: usize) -> HashMap<u64, u32> {
+    let mut axis = corners.iter().map(|c| c[index]).collect::<Vec<_>>();
+    axis.push(u64::MIN);
+    axis.push(u64::MAX);
+    axis.sort_unstable();
+    axis.dedup();
+    axis.iter()
+        .enumerate()
+        .map(|(i, x)| (*x, i as u32))
+        .collect()
 }
 
-fn parse_input(input: &str) -> Vec<(u64, u64)> {
+fn parse_input(input: &str) -> Vec<Tile> {
     input
         .lines()
         .map(|line| {
             let (l, r) = line.split_once(',').unwrap();
-            (l.parse().unwrap(), r.parse().unwrap())
+            [l.parse().unwrap(), r.parse().unwrap()]
         })
         .collect()
 }
